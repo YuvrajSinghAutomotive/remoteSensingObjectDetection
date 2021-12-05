dc36073fb83a409f4770e3d1eab6b9474776ebab
Yuvraj Singh <yuvraj.controlsdev@gmail.com>
2021-12-05 18:05:20 -0500

    U-Net example complete

--- UNet Example/data.py
+++ UNet Example/data.py
@@ -2,6 +2,11 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import cv2
+
+H = 256
+W = 256
 
 def process_data(data_path,file_path):
     df = pd.read_csv(file_path,sep=' ',header=None)
@@ -19,8 +24,47 @@ def load_data(path):
     train_y,valid_y = train_test_split(train_y,test_size=0.2,random_state=42)
     return (train_x,train_y),(valid_x,valid_y),(test_x,test_y)
 
+def read_image(x):
+    x = cv2.imread(x,cv2.IMREAD_COLOR)
+    x = cv2.resize(x,(W,H))
+    x = x/255.0
+    x = x.astype(np.float32)
+    return x
+
+def read_mask(x):
+    x = cv2.imread(x,cv2.IMREAD_GRAYSCALE)
+    x = cv2.resize(x,(W,H))
+    x = x-1
+    x = x.astype(np.int32)
+    return x
+
+def tf_dataset(x,y,batch=8):
+    dataset = tf.data.Dataset.from_tensor_slices((x,y))
+    dataset = dataset.shuffle(buffer_size=5000)
+    dataset = dataset.map(preprocess)
+    dataset = dataset.batch(batch)
+    dataset = dataset.prefetch(2)
+    return dataset
+
+def preprocess(x,y):
+    def f(x,y):
+        x = x.decode()
+        y = y.decode()
+        image = read_image(x)
+        mask = read_mask(y)
+        return image,mask
+    image,mask = tf.numpy_function(f,[x,y],[tf.float32,tf.int32])
+    mask = tf.one_hot(mask,3,dtype=tf.int32)
+    image.set_shape([H,W,3])
+    mask.set_shape([H,W,3])
+    return image,mask
 
 if __name__=="__main__":
     path = "oxford-iiit-pet/"
     (train_x,train_y),(valid_x,valid_y),(test_x,test_y) = load_data(path)
-    print(f"Fataset: Train: {len(train_x)}, Test: {len(test_x)}, Validation: {len(valid_x)}")
+    print(f"Dataset: Train: {len(train_x)}, Test: {len(test_x)}, Validation: {len(valid_x)}")
+
+    dataset = tf_dataset(train_x,train_y,batch=8)
+    for x,y in dataset:
+        print(x.shape,y.shape)
+        
